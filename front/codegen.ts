65a86d7da8557d52fa5b9cc30d03660e30c732cf
Paul Decauchy <paul.decauchy@epitech.eu>
2025-10-14 14:40:20 +0200

    fix CI

--- front/codegen.ts
+++ front/codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://localhost:8084/query",
+  schema: process.env.SCHEMA_URL,
   documents: "apollo/**/*.graphql",
   generates: {
     "generated/": {
