5d22bbf350fcfa139389689025193b0399f31d08
henri <henri.levraut@epitech.eu>
2026-01-05 15:26:48 +0100

    Me page rendering

--- front/app/dashboard/me/page.tsx
+++ front/app/dashboard/me/page.tsx
@@ -1,7 +1,7 @@
 import { ProfileInfo } from "@/app/dashboard/me/profile-info";
 import { DeleteAccount } from "@/app/dashboard/me/delete-account";
 
-export default function Me() {
+export default async function Me() {
   return (
     <div className="flex flex-col justify-center items-centerflex-col gap-4 p-4 pt-0">
       <div className="max-w-4/5 min-w-4/5 m-auto">
